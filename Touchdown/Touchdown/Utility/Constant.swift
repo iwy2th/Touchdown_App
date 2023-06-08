7a5a4cac5600097fe4c983f0b8ce8280fc5fae20
iwy2th <huyloind03@gmail.com>
2023-06-08 21:43:15 +0700

    add FooterView

---
+++ Touchdown/Touchdown/Utility/Constant.swift
@@ -0,0 +1,26 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Iwy2th on 6/8/23.
+//
+
+import SwiftUI
+
+//DATA
+
+//COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(.systemGray4)
+//LAYOUT
+
+//API
+
+
+//IMAGE
+
+//FONT
+
+//STRING
+//MISC
