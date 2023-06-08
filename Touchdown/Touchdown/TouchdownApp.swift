5b142f33fc5247f9ed59acb8d913864781d7f960
iwy2th <huyloind03@gmail.com>
2023-06-08 20:32:13 +0700

    Setup and add IconApp

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Iwy2th on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
